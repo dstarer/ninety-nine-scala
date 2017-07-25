ae4c73c423ffbb70d0e8534386240edd629e8df7
chenan <anchencn@126.com>
2017-07-25 14:01:09 +0800

    solutions and test case for 16 to 22

--- src/test/scala/NinetyNineTest.scala
+++ src/test/scala/NinetyNineTest.scala
@@ -54,25 +54,25 @@ class NinetyNineTest extends FunSuite {
         val a = new P05
         val ls = List(7, 8, 0, "4", "6")
         println(a.reverseRecursive(ls))
-        
+
         assert(a.reverseRecursive(ls).equals(ls.reverse))
-        
+
         assert(a.reverseFunctional(ls).equals(ls.reverse))
-        
+
         assert(a.reverseFunctionalLeft(ls).equals(ls.reverse))
     }
-    
+
     test("P06") {
         val a = new P06
         val ls1 = List(1, 2, 2, 1)
-        
+
         assert(a.isPalindromeRecursive(ls1).equals(true))
         val ls2 = List(1, 2, 3, 2, 1)
         assert(a.isPalindromeRecursive(ls2).equals(true))
         var ls3 = List(1, 4, 2, 1)
         assert(a.isPalindromeRecursive(ls3).equals(false))
     }
-    
+
     test("P07") {
         val a = new P07
         val ls1 = List(List(1, 2), List(3, 4), List(5, List(6, 7)))
@@ -80,57 +80,101 @@ class NinetyNineTest extends FunSuite {
         println(ls1.flatten)
         println(a.flatten(ls1))
     }
-    
+
     test("P08") {
         val a = new P08
         val ls1 = List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')
         println(a compress (ls1))
         println(a compressFunctional ls1)
     }
-    
+
     test("p09") {
         val a = new P09
         val ls1 = List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')
         println(a pack ls1)
         println(a packRecursive ls1)
     }
-    
+
     test("P10") {
-        val a= new P09
+        val a = new P09
         val ls1 = List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')
         println(a encodeWithRunLength (ls1))
-        println(a encodeSimpler(ls1))
+        println(a encodeSimpler (ls1))
     }
-    
+
     test("p11") {
-        val a= new P09
+        val a = new P09
         val ls1 = List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')
-        println(a encodeWithSimpleRunLength  (ls1))
-        println(a encodefor11(ls1))
+        println(a encodeWithSimpleRunLength (ls1))
+        println(a encodefor11 (ls1))
     }
-    
+
     test("p12") {
         val a = new P09
         val ls2 = List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))
-        println(a decode(ls2))
+        println(a decode (ls2))
     }
-    
+
     test("p13") {
-        val a= new P09
+        val a = new P09
         val ls1 = List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')
         println(a encodeWithRunLength (ls1))
     }
-    
+
     test("P14") {
         val a = new P09
         val ls1 = List('a, 'b, 'c, 'c, 'd)
-        println(a duplicateElements(ls1))
+        println(a duplicateElements (ls1))
     }
-    
+
     test("P15") {
         val a = new P09
         val ls1 = List('a, 'b, 'c, 'c, 'd)
-        println(a duplicateElementsNth (ls1, 3))
+        println(a duplicateElementsNth(ls1, 3))
+    }
+
+    test("P16") {
+        val a = new P09
+        val ls1 = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+        println(a dropEveryNth(3, ls1))
+    }
+
+    test("P17") {
+        val a = new P09
+        val lst1 = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+        println(a splitTwoParts(3, lst1))
+        println(a splitTwoFunction(3, lst1))
+    }
+
+    test("P18") {
+        val a = new P09
+        val lst1 = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+        println(a extractSlice(3, 7, lst1))
+    }
+
+    test("P19") {
+        val a = new P09
+        val lst1 = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+        println(a rotate(3, lst1))
+        println(a rotate(-2, lst1))
+    }
+
+    test("P20") {
+        val a = new P09
+        val lst1 = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+        println(a removeKth(2, lst1))
+    }
+
+    test("P21") {
+        val a = new P09
+        val lst1 = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+        println(a insertKth(3, 't, lst1))
+    }
+
+    test("P22") {
+        val a = new P09
+        println(a range(4, 9))
+        println(a rangeFunctional(4, 9))
     }
 }
 
