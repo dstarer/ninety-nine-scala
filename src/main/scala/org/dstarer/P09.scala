f8911b85af71b50f68754e41118b35dfa4e8bf98
chenan <anchencn@126.com>
2017-07-25 16:53:44 +0800

    solutions and test case for 16 to 22

--- src/main/scala/org/dstarer/P09.scala
+++ src/main/scala/org/dstarer/P09.scala
@@ -1,5 +1,7 @@
 package org.dstarer
 
+import java.util.NoSuchElementException
+
 /**
   * Created by huberg on 17-7-19.
   */
@@ -16,7 +18,7 @@ class P09 {
             }
         }
     }
-    
+
     /**
       *
       * using span filter part the list into two lists.
@@ -36,7 +38,7 @@ class P09 {
                 packed :: packRecursive(next)
         }
     }
-    
+
     def encodeWithRunLength[A](ls: List[A]): List[Any] = {
         if (ls.isEmpty)
             List()
@@ -52,7 +54,7 @@ class P09 {
             }
         }
     }
-    
+
     def encodeWithSimpleRunLength[A](ls: List[A]): List[Any] = {
         if (ls.isEmpty)
             List()
@@ -72,14 +74,14 @@ class P09 {
                 left +: encodeWithSimpleRunLength(next)
         }
     }
-    
+
     def encodeSimpler[A](ls: List[A]): List[Any] = {
         val packed = packRecursive(ls)
         packed map {
             e => (e.length, e.head)
         }
     }
-    
+
     def encodefor11[A](ls: List[A]): List[Any] = {
         packRecursive(ls) map {
             e => {
@@ -90,7 +92,7 @@ class P09 {
             }
         }
     }
-    
+
     def make[A](count: Int, v: A): List[A] = {
         var ls = List[A]()
         var index: Int = 0
@@ -99,22 +101,112 @@ class P09 {
         }
         ls
     }
-    
+
     def decode[A](ls: List[(Int, A)]): List[A] = {
         ls flatMap {
             e => make(e._1, e._2)
         }
     }
-    
+
     def duplicateElements[A](ls: List[A]): List[A] = {
         ls flatMap {
             e => make(2, e)
         }
     }
-    
-    def duplicateElementsNth[A] (ls: List[A], n: Int): List[A] = {
+
+    def duplicateElementsNth[A](ls: List[A], n: Int): List[A] = {
         ls flatMap {
             e => make(n, e)
         }
     }
+
+    def dropEveryNth[A](n: Int, ls: List[A]): List[A] = {
+        val tmpZip1: List[(A, Int)] = ls.zip(ls.indices)
+        val tmpZip2: List[(A, Int)] = tmpZip1.filter(a => a._2 % n != n - 1)
+        val (res, _) = tmpZip2.unzip
+        res
+    }
+
+    def dropFunctions[A](n: Int, ls: List[A]): List[A] = {
+        ls.zipWithIndex filter {
+            v => (v._2 + 1) % n != 0
+        } map {
+            _._1
+        }
+    }
+
+    def splitTwoParts[A](n: Int, ls: List[A]): (List[A], List[A]) = {
+        val tmp = ls.zipWithIndex partition {
+            v => (v._2 < n)
+        }
+        (tmp._1.map(_._1), tmp._2.map(_._1))
+    }
+
+    def splitTwoFunction[A](n: Int, ls: List[A]): (List[A], List[A]) = {
+        (ls.take(n), ls.drop(n))
+    }
+
+    def extractSlice[A](l: Int, k: Int, ls: List[A]): List[A] = {
+        ls match {
+            case ls if (ls.length < l) => Nil
+            case ls if (ls.length < k) => ls.drop(l)
+            case _ => ls.take(k).drop(l)
+        }
+    }
+
+    def rotate[A](l: Int, ls: List[A]): List[A] = {
+        (l, ls) match {
+            case (_, Nil) => Nil
+            case (l, ls) => {
+                if (l == 0) ls
+                else if (l < 0) ls.takeRight(-l) ::: ls.dropRight(-l)
+                else ls.drop(l) ::: ls.take(l)
+            }
+        }
+    }
+
+    def removeKth[A](l: Int, ls: List[A]): (List[A], A) = {
+        if (l < 0)
+            throw new NoSuchElementException
+        else (l, ls) match {
+            case (_, Nil) => throw new NoSuchElementException
+            case (0, h :: tail) => (tail, h)
+            case (_, h :: tail) => {
+                val (t, e) = removeKth(l - 1, tail)
+                (h :: t, e)
+            }
+        }
+    }
+
+    def insertKth[A](l: Int, e: A, ls: List[A]): List[A] = {
+        if (l > ls.length || l < 0)
+            throw new IndexOutOfBoundsException
+        (ls.take(l) :+ e) ::: ls.drop(l)
+    }
+
+    def range(start: Int, end: Int): List[Int] = {
+        if (start > end)
+            throw new IllegalArgumentException
+        var ls: List[Int] = List[Int]()
+        var i = 0
+        for (i <- start to (end))
+            ls = ls :+ i
+        ls
+    }
+
+    def unfoldRight[A, B](s: B)(f: B => Option[(A, B)]): List[A] = {
+        f(s) match {
+            case None => Nil
+            case Some((r, n)) => r :: unfoldRight(n)(f)
+        }
+    }
+
+    def rangeFunctional(start: Int, end: Int): List[Int] = {
+        unfoldRight(start) {
+            n =>
+                if (n > end) None
+                else Some(n, n + 1)
+        }
+    }
+
 }
